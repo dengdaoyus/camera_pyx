724bc330932f0a0a56300c3641acf7cce6bd66b8
584076388@qq.com <pyx1994512>
2017-12-11 15:04:57 +0800

    更新单选 UI 可以从相机选择 并剪裁

--- app/src/main/java/com/pyx/cameraprojcet/MainActivity.java
+++ app/src/main/java/com/pyx/cameraprojcet/MainActivity.java
@@ -35,8 +35,9 @@ public class MainActivity extends AppCompatActivity implements PyxCamera.CameraI
     //一、实例化对象设置info对象设置SDK参数及UI
     private void initCamera() {
         info = new CameraSdkParameterInfo();
-        info.setSingleMode(true);
-        info.setCutoutImage(true);
+        info.setMaxImage(6);
+        info.setSingleMode(false);
+        info.setCutoutImage(false);
         pyxCamera = new PyxCamera(this, info, this);
     }
 
